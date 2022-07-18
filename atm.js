e407dae52350e8f8b1fb35a62865fed5893c3b48
Balbin <xxbalbin@gmail.com>
2022-07-18 18:44:23 -0500

    algorithm added

--- atm.js
+++ atm.js
@@ -1,1 +1,44 @@
-alert("oe");
+class Bill
+{
+    constructor(v, a)
+    {
+        this.value = v;
+        this.amount = a;
+    }
+}
+
+function cashWithdrawal()
+{
+    for(let bi of cash)
+    {
+        if(money > 0)
+        {
+            div = Math.floor(money / bi.value);
+            if(div > bi.amount)
+            {
+                paper = bi.amount;
+            }
+            else
+            {
+                paper = div;
+            }
+
+            withdrawn.push(new Bill(bi.value, paper));
+        }
+    }
+}
+
+let cash = [];
+let withdrawn = [];
+cash.push(new Bill(50, 3));
+cash.push(new Bill(20, 2));
+cash.push(new Bill(10, 3));
+
+let money = 210;
+let div = 0;
+let paper = 0;
+
+let b = document.getElementById("withdraw");
+b.addEventListener("click", cashWithdrawal);
+
+
