15b001d83da2fdd8b463e9486f6dc13c9629ae03
Balbin <xxbalbin@gmail.com>
2022-07-18 19:10:20 -0500

    functional code(1)

--- atm.js
+++ atm.js
@@ -9,6 +9,8 @@ class Bill
 
 function cashWithdrawal()
 {
+    let t = document.getElementById("moneyTo")
+    money = parseInt(t.value);
     for(let bi of cash)
     {
         if(money > 0)
@@ -24,17 +26,29 @@ function cashWithdrawal()
             }
 
             withdrawn.push(new Bill(bi.value, paper));
+            money = money - (bi.value * paper);
+        }
+    }
+    if(money > 0)
+    {
+        document.write("Im a poor ATM");
+    }
+    else
+    {
+        for(let w of withdrawn)
+        {
+            document.write(w.amount + " Bills of $" + w.value + "<br />");
         }
     }
 }
 
 let cash = [];
 let withdrawn = [];
-cash.push(new Bill(50, 3));
-cash.push(new Bill(20, 2));
-cash.push(new Bill(10, 3));
+cash.push(new Bill(50, 10));
+cash.push(new Bill(20, 30));
+cash.push(new Bill(10, 10));
 
-let money = 210;
+let money = 0;
 let div = 0;
 let paper = 0;
 
